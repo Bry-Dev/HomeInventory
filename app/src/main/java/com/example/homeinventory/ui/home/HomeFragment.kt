82fed5a57e30c91ba5f80307d57a831f5bac8d41
Bry-Dev <43851087+Bry-Dev@users.noreply.github.com>
2021-04-20 15:21:38 +0800

    Change for Beta Release

--- app/src/main/java/com/example/homeinventory/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/homeinventory/ui/home/HomeFragment.kt
@@ -2,20 +2,25 @@ package com.example.homeinventory.ui.home
 
 import android.os.Bundle
 import android.view.*
-import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.homeinventory.R
+import com.example.homeinventory.helper.EditDialog
 import com.example.homeinventory.model.Category
 import com.example.homeinventory.model.HomeItem
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class HomeFragment : Fragment(), OnClickItem, OnLongClick {
 
     private val homeViewModel: HomeViewModel by viewModels()
+
+    companion object {
+        private const val TAG = "HomeFragment"
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,6 +31,7 @@ class HomeFragment : Fragment(), OnClickItem, OnLongClick {
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val categoryRecyclerView = root.findViewById<RecyclerView>(R.id.recycler_category)
         val adapter = CategoryRecyclerViewAdapter(this, this)
+        categoryRecyclerView.itemAnimator = null
         categoryRecyclerView.adapter = adapter
         homeViewModel.allHomeItem.observe(viewLifecycleOwner, {
             it?.let {
@@ -36,12 +42,17 @@ class HomeFragment : Fragment(), OnClickItem, OnLongClick {
     }
 
     override fun onEditClick(homeItem: HomeItem) {
+        EditDialog(homeItem).show(childFragmentManager, TAG)
+    }
+
+    override fun onLongEditClick(homeItem: HomeItem): Boolean {
         val action = HomeFragmentDirections.actionNavigationHomeToNavigationAddHomeItem()
         action.homeItem = homeItem
         findNavController().navigate(action)
+        return true
     }
 
-    override fun onLongCategoryClick(category: Category) : Boolean {
+    override fun onLongCategoryClick(category: Category): Boolean {
         val action = HomeFragmentDirections.actionNavigationHomeToNavigationAddCategory()
         action.category = category
         findNavController().navigate(action)
@@ -65,12 +76,6 @@ class HomeFragment : Fragment(), OnClickItem, OnLongClick {
         }
         return super.onOptionsItemSelected(item)
     }
-
-    companion object {
-        private const val TAG = "HomeFragment"
-    }
-
-
 
 
 }
