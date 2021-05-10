0c4663bff315be33778145c0512361d8164913c5
Bry-Dev <43851087+Bry-Dev@users.noreply.github.com>
2021-05-10 18:05:23 +0800

    Change date color of overdue shopping list

--- app/src/main/java/com/example/homeinventory/ui/shopping/AddShoppingFragment.kt
+++ app/src/main/java/com/example/homeinventory/ui/shopping/AddShoppingFragment.kt
@@ -8,7 +8,6 @@ import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
 import com.example.homeinventory.R
 import com.example.homeinventory.model.ShoppingItem
 import com.example.homeinventory.ui.home.HomeViewModel
--- app/src/main/java/com/example/homeinventory/ui/shopping/ShoppingBindingUtils.kt
+++ app/src/main/java/com/example/homeinventory/ui/shopping/ShoppingBindingUtils.kt
@@ -1,8 +1,11 @@
 package com.example.homeinventory.ui.shopping
 
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import com.example.homeinventory.R
 import com.example.homeinventory.model.ShoppingForHome
+import java.time.LocalDate
 
 @BindingAdapter("shoppingItemName")
 fun TextView.setShoppingName(shoppingForHome : ShoppingForHome) {
@@ -21,6 +24,9 @@ fun TextView.setShoppingQty(shoppingForHome : ShoppingForHome) {
 @BindingAdapter("shoppingItemDate")
 fun TextView.setShoppingDate(shoppingForHome : ShoppingForHome) {
     shoppingForHome.let{
-        text = it.shoppingItem.itemBuyDate.toString()
+        val shopDate = it.shoppingItem.itemBuyDate.toString().trim()
+        text = shopDate
+        val toDate = LocalDate.now()
+        if (LocalDate.parse(shopDate) < toDate) setTextColor(ContextCompat.getColor(context, R.color.attention))
     }
 }
